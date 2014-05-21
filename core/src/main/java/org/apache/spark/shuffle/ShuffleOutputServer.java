8df7ad30c2b1a772875a7663f38ad25306f0edec
chengxiang li <chengxiang.li@intel.com>
2014-05-21 14:48:11 +0800

    add parallel write data for reduce side.

--- core/src/main/java/org/apache/spark/shuffle/ShuffleOutputServer.java
+++ core/src/main/java/org/apache/spark/shuffle/ShuffleOutputServer.java
@@ -11,11 +11,19 @@ import java.net.ServerSocket;
 import java.nio.ByteBuffer;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
+import java.util.*;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 
 public class ShuffleOutputServer {
     Logger log = LoggerFactory.getLogger(this.getClass());
     ServerSocketChannel listener = null;
+    Executor executor = Executors.newFixedThreadPool(4);
+    ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
+    Map<String, Queue<String>> allAvailableFiles = new HashMap<String, Queue<String>>();
+    private int MAX_REDUCE_FILE_NUMBER_PER_PARTITION = 4;
 
     public void mySetup() {
         InetSocketAddress listenAddr = new InetSocketAddress(9026);
@@ -40,56 +48,99 @@ public class ShuffleOutputServer {
     }
 
     public void readData() {
-        ByteBuffer dst = ByteBuffer.allocate(4096);
+
         try {
             while (true) {
                 SocketChannel conn = listener.accept();
                 System.out.println("Accepted : " + conn);
                 conn.configureBlocking(true);
-
-                int nread = conn.read(dst);
-                dst.rewind();
-                int size = dst.getInt();
-                byte[] pathByte = new byte[size];
-                dst.get(pathByte);
-                String path = new String(pathByte);
-                System.out.println("start to write file:" + path);
-                File file = new File(path);
-                if(file.exists()) {
-                    file.delete();
-                } else {
-                    File parent = new File(file.getParent());
-                    if (!parent.exists()) {
-                        parent.mkdirs();
-                    }
-                    file.createNewFile();
-                }
-                FileOutputStream outputStream = new FileOutputStream(file, false);
-                int len = nread - 4 - size;
-                if(len >0) {
-                    outputStream.write(dst.array(), 4 + size, len);
-                }
-                nread = 0;
-                while (nread != -1) {
-                    dst.rewind();
-                    byte[] array = dst.array();
-                    System.out.printf("array size[%d], nread[%d]\n", array.length, nread);
-                    outputStream.write(array, 0, nread);
-                    dst.clear();
-                    try {
-                        nread = conn.read(dst);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                        nread = -1;
-                    }
-                }
-
-                outputStream.flush();
-                outputStream.close();
-                System.out.println("finished write file:" + path);
+                executor.execute(new OutputPersistentRunnable(conn));
             }
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    class OutputPersistentRunnable implements Runnable {
+
+        private SocketChannel conn;
+
+        public OutputPersistentRunnable(SocketChannel conn) {
+            this.conn = conn;
+        }
+
+        @Override
+        public void run() {
+            try {
+                readOutput(conn);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void readOutput(SocketChannel conn) throws IOException {
+        ByteBuffer dst = ByteBuffer.allocate(4096000);
+        int nread = conn.read(dst);
+        dst.rewind();
+        int size = dst.getInt();
+        byte[] pathByte = new byte[size];
+        dst.get(pathByte);
+        String path = new String(pathByte);
+        System.out.println("start to write file:" + path);
+        File file = new File(path);
+        String fileParent = file.getParent();
+        File parent = new File(fileParent);
+        if (!parent.exists()) {
+            parent.mkdirs();
+        }
+        try {
+            lock.writeLock().lock();
+            Queue<String> availableFiles = allAvailableFiles.get(fileParent);
+            if (availableFiles == null) {
+                availableFiles = new LinkedList<String>();
+                for (int i=0; i<MAX_REDUCE_FILE_NUMBER_PER_PARTITION; i++) {
+                    availableFiles.add(Integer.toString(i));
+                }
+                allAvailableFiles.put(fileParent, availableFiles);
+            }
+            String fileName = availableFiles.poll();
+            file = new File(parent, fileName);
+        } finally {
+            lock.writeLock().unlock();
+        }
+        if(file.exists()) {
+            file.createNewFile();
+        }
+        FileOutputStream outputStream = new FileOutputStream(file, false);
+        int len = nread - 4 - size;
+        if (len > 0) {
+            outputStream.write(dst.array(), 4 + size, len);
+        }
+        nread = 0;
+        while (nread != -1) {
+            dst.rewind();
+            byte[] array = dst.array();
+            System.out.printf("array size[%d], nread[%d]\n", array.length, nread);
+            outputStream.write(array, 0, nread);
+            dst.clear();
+            try {
+                nread = conn.read(dst);
+            } catch (IOException e) {
+                e.printStackTrace();
+                nread = -1;
+            }
+        }
+
+        outputStream.flush();
+        outputStream.close();
+        System.out.println("finished write file:" + path);
+        try {
+            lock.writeLock().lock();
+            Queue<String> availableFiles = allAvailableFiles.get(fileParent);
+            availableFiles.add(file.getName());
+        } finally {
+            lock.writeLock().unlock();
         }
     }
 }
